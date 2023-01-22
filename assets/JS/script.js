c8f9d6fd78391104d6102670476815fa5e2977a8
Fabio Aurora <fabioauroradev@gmail.com>
2023-01-22 14:32:40 +0000

    completed hero section with effects on content text

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,8 +3,38 @@
 const navBurger = document.querySelector('.navbar__hamburger');
 const navMenu = document.querySelector('.navbar__menu');
 
-
+/* Menu toggle class and translate animation */
+let menuActive = false;
 navBurger.addEventListener('click', () => {
-    navBurger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  menuActive = !menuActive;
+  navBurger.classList.toggle('active', menuActive);
+  navMenu.classList.toggle('active', menuActive);
+  if(!menuActive){
+    navMenu.classList.add('inactive')
+  }else{
+    navMenu.classList.remove('inactive')
+  }
+});
+
+//remove the inactive class after the transition is finished:
+navMenu.addEventListener('transitionend', (e) => {
+    if(e.propertyName === 'transform' && !menuActive) {
+        navMenu.classList.remove('inactive');
+    }
 })
+
+
+
+/* animated elements from right to left when the DOM elements are loaded */
+document.addEventListener('DOMContentLoaded', () => {
+    let elements = document.querySelectorAll('.animated-element');
+    let delay = 0;
+    let interval = .5;
+
+    elements.forEach((element, index) => {
+        setTimeout(() => {
+            element.style.animationDelay = delay + (interval * index) + 's';
+            element.classList.add('animate');
+        }, delay * 1000);
+    });
+});
